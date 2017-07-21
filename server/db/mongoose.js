66d027facfdb42b03b22dfcf7c44dd5f792dfa24
El Mahdi SYOUMI <mahdi.syoumi@gmail.com>
2017-07-21 16:39:11 +0100

    Heroku issues

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp';
+const dbURI = process.env.MY_MONGO_DB_URI || 'mongodb://127.0.0.1:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(dbURI, {useMongoClient: true});
